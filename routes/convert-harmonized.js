204bb8ab93fa62ebecb11fd07b93f1c1f927e5a5
Brock Fanning <brockfanning@gmail.com>
2021-02-05 07:20:41 -0500

    Add PDF for harmonized conversion

--- routes/convert-harmonized.js
+++ routes/convert-harmonized.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const multer = require('multer')
-const { HarmonizedWordTemplateInput, SdmxOutput } = require('sdg-metadata-convert')
+const { HarmonizedWordTemplateInput, SdmxOutput, PdfOutput } = require('sdg-metadata-convert')
 const router = express.Router()
 const helpers = require('../lib/helpers')
 
@@ -29,19 +29,44 @@ router.post('/', upload.single('file'), async (req, res) => {
         else {
             const input = new HarmonizedWordTemplateInput()
             const sdmxOutput = new SdmxOutput()
+            const pdfOutput = new PdfOutput({
+                conceptNames: true,
+                puppeteerLaunchOptions: helpers.getPuppeteerLaunchOptions(),
+                layoutFolder: path.join(__dirname, '..', 'views'),
+                layout: 'pdf-output.njk',
+            })
 
+            const zipTempFile = path.join('user_uploads', indicator.filename + '.zip')
+            const zipDestFile = helpers.convertFilename(indicator.originalname, '.zip')
             const sdmxTempFile = path.join('user_uploads', indicator.filename + '.xml')
             const sdmxDestFile = helpers.convertFilename(indicator.originalname, '.xml')
+            const pdfTempFile = path.join('user_uploads', indicator.filename + '.pdf')
+            const pdfDestFile = helpers.convertFilename(indicator.originalname, '.pdf')
 
+            let messages
             input.read(indicator.path, descriptors)
                 .then(metadata => sdmxOutput.write(metadata, sdmxTempFile))
-                .then(metadata => res.send({
+                .then(metadata => pdfOutput.write(metadata, pdfTempFile))
+                .then(metadata => {
+                    messages = metadata.getMessages()
+                    return helpers.zipOutputFiles(zipTempFile, [
+                        {
+                            from: sdmxTempFile,
+                            to: sdmxDestFile,
+                        },
+                        {
+                            from: pdfTempFile,
+                            to: pdfDestFile,
+                        }
+                    ])
+                })
+                .then(() => res.send({
                     status: true,
                     message: 'Indicator successfully converted.',
                     data: {
-                        filePath: sdmxTempFile,
-                        downloadName: sdmxDestFile,
-                        warnings: metadata.getMessages(),
+                        filePath: zipTempFile,
+                        downloadName: zipDestFile,
+                        warnings: messages,
                     }
                 }))
                 .catch(err => {
